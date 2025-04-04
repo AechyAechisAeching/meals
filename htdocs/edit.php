0c9ab5fd8a3d75e9bd6fca1754c58e06129cc4a9
Aech <vincentbakker.08@gmail.com>
2025-04-04 11:45:05 +0200

    recipe name form control

--- htdocs/edit.php
+++ htdocs/edit.php
@@ -12,3 +12,27 @@ echo "<pre>";
 print_r(value: $recset);
 echo "</pre>";
 ?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Recept</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.4/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-DQvkBjpPgn7RC31MCQoOeC9TI2kdqa4+BSgNMNj8v77fdC77Kj5zpWFTJaaAoMbC" crossorigin="anonymous">
+</head>
+<body>
+<div class="container">
+    <form>
+<div class="mb-3">
+    <label for="name" class="form-label">Receptnaam</label>
+    <input type="email" class="form-control" id="name" value="<?php echo $recset[0]['recipe_name'] ?>">
+</div>
+<div class="mb-3">
+    <label for="exampleFormControlTextarea1" class="form-label">Example textarea</label>
+    <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+    </div>
+    </form>
+    </div>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.4/dist/js/bootstrap.bundle.min.js" integrity="sha384-YUe2LzesAfftltw+PEaao2tjU/QATaW/rOitAq67e0CT0Zi2VVRL0oC4+gAaeBKu" crossorigin="anonymous"></script>
+</body>
+</html>
